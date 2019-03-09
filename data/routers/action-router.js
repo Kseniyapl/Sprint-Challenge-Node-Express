8549a4fb72a8149f7d38c9f55e5571393ecabd86
Kseniya Platonava <ksu2008@gmail.com>
2019-03-08 22:50:34 -0500

    Add POST, PUT, DELTE And GET By ID Action Endpoints

--- data/routers/action-router.js
+++ data/routers/action-router.js
@@ -15,4 +15,80 @@ router.get('/', async (req, res) => {
   }
 });
 
+//POST
+
+router.post('/', async (req, res) => {
+
+  if (!req.body) {
+     res.status(400).json({ errorMessage: "Please provide an action." });
+ } else {    
+     try {
+         const action = await actionDB.insert(req.body);
+         res.status(201).json(action);
+     } catch (err) {
+      res.status(500).json({error: "There was an error while saving the action."});
+    }
+ }
+
+
+});
+
+//PUT (Update)
+
+router.put('/:id', async (req, res) => {
+
+  
+  try {
+      const action = await actionDB.update(req.params.id, req.body);
+
+       if (action) {
+          res.status(200).json(action);
+      } else {
+          res.status(404).json({ message: "The action with the specified ID does not exist." });
+      }
+  } catch (error) {
+      // log error to database
+      console.log(error);
+      res.status(500).json({ error: "The action information could not be modified." });
+  };
+ });
+
+
+//DELETE
+
+router.delete('/:id', async (req, res) => {
+try {
+   const action = await actionDB.remove(req.params.id);
+
+    if (action > 0) {
+       res.status(200).json({ message: "The action has been removed" });
+   } else {
+       res.status(404).json({ message: "The action with the specified ID does not exist." });
+   }
+} catch (error) {
+   // log error to database
+   console.log(error);
+   res.status(500).json({ error: "The action could not be removed" });
+}
+});
+
+
+//GET by ID
+router.get('/:id', async (req, res) => {
+try {
+
+ const action = await actionDB.get(req.params.id);
+
+ if (action) {
+   res.status(200).json(action);
+} else {
+   res.status(404).json({ message: "The action with the specified ID does not exist." });
+}
+} catch (error) {
+// log error to database
+console.log(error);
+res.status(500).json({ error: "The action could not be retrieved." });
+} 
+});
+
 module.exports = router;
