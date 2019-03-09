1a4ac67b3b7d1af6091c37aa3875ffa7c79c9af2
Kseniya Platonava <ksu2008@gmail.com>
2019-03-08 22:38:14 -0500

    Add Action GET Endpoint

--- data/routers/action-router.js
+++ data/routers/action-router.js
@@ -4,6 +4,15 @@ const actionDB = require('../helpers/actionModel.js');
 
 const router = express.Router();
 
-
+router.get('/', async (req, res) => {
+  try {
+      const action = await actionDB.get();
+      res.status(200).json(action);
+  } catch (error) {
+     // log error to database
+      console.log(error);
+      res.status(500).json({ error: "The action information could not be retrieved." });
+  }
+});
 
 module.exports = router;
